9b914b86ceb8e8e6225ded832b22db2784369a4d
Andrew <uytov1991@gmail.com>
2021-06-11 14:43:51 +0300

    добавил ссылки

--- src/js/range.js
+++ src/js/range.js
@@ -2,52 +2,55 @@ const MAX_VALUE = 300000
 
 const rangeInputsWrapper = document.querySelector('.range-inputs-wrapper')
 
-const minInput = rangeInputsWrapper.querySelector('.price-input--min')
-const maxInput = rangeInputsWrapper.querySelector('.price-input--max')
-const rangeWrapper = rangeInputsWrapper.querySelector('.range__wrapper')
+if (rangeInputsWrapper) {
+  const minInput = rangeInputsWrapper.querySelector('.price-input--min')
+  const maxInput = rangeInputsWrapper.querySelector('.price-input--max')
+  const rangeWrapper = rangeInputsWrapper.querySelector('.range__wrapper')
 
-rangeWrapper.addEventListener('pointerdown', (evt) => {
-  let toggle = evt.target.closest('.range__toggle')
-  if (!toggle) return
-  toggle.ondragstart = () => false
-  toggle.style.touchAction = 'none'
-  toggle.setPointerCapture(evt.pointerId)
+  rangeWrapper.addEventListener('pointerdown', (evt) => {
+    let toggle = evt.target.closest('.range__toggle')
+    if (!toggle) return
+    toggle.ondragstart = () => false
+    toggle.style.touchAction = 'none'
+    toggle.setPointerCapture(evt.pointerId)
 
-  let mutableProperty
-  toggle.classList.contains('range__toggle--left')
-    ? (mutableProperty = '--min-position')
-    : (mutableProperty = '--max-position')
+    let mutableProperty
+    toggle.classList.contains('range__toggle--left')
+      ? (mutableProperty = '--min-position')
+      : (mutableProperty = '--max-position')
 
-  let shiftX = evt.clientX - toggle.getBoundingClientRect().left
-  let newPosition
-  const rangeWrapperWidth = rangeWrapper.getBoundingClientRect().width
-  let rightEdge = rangeWrapperWidth - 24
-  let step = 100 / rightEdge
-  let input
-  if (toggle.classList.contains('range__toggle--left')) {
-    input = minInput
-  } else {
-    input = maxInput
-  }
-
-  const moveAt = (evt) => {
-    newPosition = evt.pageX - shiftX - rangeWrapper.getBoundingClientRect().left
-    if (newPosition < 0) newPosition = 0
-    if (newPosition > rightEdge) {
-      newPosition = rightEdge
+    let shiftX = evt.clientX - toggle.getBoundingClientRect().left
+    let newPosition
+    const rangeWrapperWidth = rangeWrapper.getBoundingClientRect().width
+    let rightEdge = rangeWrapperWidth - 24
+    let step = 100 / rightEdge
+    let input
+    if (toggle.classList.contains('range__toggle--left')) {
+      input = minInput
+    } else {
+      input = maxInput
     }
 
-    rangeWrapper.style.setProperty(mutableProperty, `${newPosition}px`)
+    const moveAt = (evt) => {
+      newPosition =
+        evt.pageX - shiftX - rangeWrapper.getBoundingClientRect().left
+      if (newPosition < 0) newPosition = 0
+      if (newPosition > rightEdge) {
+        newPosition = rightEdge
+      }
 
-    input.value = Math.ceil(newPosition * step) * (MAX_VALUE / 100)
-  }
+      rangeWrapper.style.setProperty(mutableProperty, `${newPosition}px`)
 
-  toggle.addEventListener('pointermove', moveAt)
+      input.value = Math.ceil(newPosition * step) * (MAX_VALUE / 100)
+    }
 
-  const mouseUp = (evt) => {
-    toggle.removeEventListener('pointermove', moveAt)
-    toggle.removeEventListener('pointerup', mouseUp)
-  }
+    toggle.addEventListener('pointermove', moveAt)
 
-  toggle.addEventListener('pointerup', mouseUp)
-})
+    const mouseUp = (evt) => {
+      toggle.removeEventListener('pointermove', moveAt)
+      toggle.removeEventListener('pointerup', mouseUp)
+    }
+
+    toggle.addEventListener('pointerup', mouseUp)
+  })
+}
